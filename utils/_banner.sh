24b6c74850c82fb7cca887d9a77fe83779860fff
onzepoint <129421033+onzepoint@users.noreply.github.com>
2023-09-11 02:45:33 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -37,12 +37,11 @@ print_banner() {
   printf "\n"
 
   printf "${GREEN}";
-printf "  ██████╗ ████████╗ █████╗ ██╗██╗     ██╗     ███████╗\n"
-printf " ██╔═══██╗╚══██╔══╝██╔══██╗██║██║     ██║     ██╔════╝\n"
-printf " ██║   ██║   ██║   ███████║██║██║     ██║     ███████╗\n"
-printf " ██║   ██║   ██║   ██╔══██║██║██║     ██║     ╚════██║\n"
-printf " ╚██████╔╝   ██║   ██║  ██║██║███████╗███████╗███████║\n"
-printf "  ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚═╝╚══════╝╚══════╝╚══════╝\n"
+  printf("M       M  UU   UU  L         TTTTTTT  IIIII  ZZZZZ   AAAAA  PPPPP\n");
+    printf("MM     MM  UU   UU  L           TTT      I       ZZ  A     A P   P\n");
+    printf("M M   M M  UU   UU  L           TTT      I      ZZ   AAAAAAA PPPPP\n");
+    printf("M  M M  M  UU   UU  L           TTT      I     ZZ    A     A P\n");
+    printf("M   M   M   UUUUU   LLLLLLL     TTT     IIIII  ZZZZZ  A       A P\n");
   printf "${NC}";
 
   printf "\n"
